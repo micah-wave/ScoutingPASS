0684bc7e4260afa5f917c108dd17fda3afb19cca
micah-wave <124638228+micah-wave@users.noreply.github.com>
2023-02-09 19:48:05 -0500

    reverted back to new map 1
    got rid of inorganic input 'preload piece' code

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -55,13 +55,6 @@ var config_data = `
       "min": 1,
       "max": 99999
     },
-    { "name": "Preload Piece",
-      "code": "p",
-      "type": "preload",
-      "choices": {
-        "cb1": "Cube<br>",
-        "cn1": "Cone<br>",
-      },
     { "name": "Auto Start Position",
       "code": "as",
       "type": "clickable_image",
@@ -238,5 +231,4 @@ var config_data = `
     }
   ]
 }`;
-
 
