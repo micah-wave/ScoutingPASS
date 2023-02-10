9eb345c09e8d8458db049c4994b234aa411c8561
micah-wave <124638228+micah-wave@users.noreply.github.com>
2023-02-09 20:22:27 -0500

    attempt 2 resetting and inputting new map
    reset code to where I had it before it broke aka gridmap 1, then inputted gridmap 2

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -58,7 +58,7 @@ var config_data = `
     { "name": "Auto Start Position",
       "code": "as",
       "type": "clickable_image",
-      "filename": "2023/field_image.png",
+      "filename": "2023/new gridmap2.jpg",
       "clickRestriction": "one",
       "shape": "circle 5 black red true"
     }
@@ -123,3 +123,111 @@ var config_data = `
     { "name": "Who Defended this bot",
       "code": "who",
       "type": "text"
+    },
+    { "name": "Smart Placement<br>(creates Links)",
+      "code": "lnk",
+      "type": "bool"
+    },
+    { "name": "Floor Pickup",
+      "code": "fpu",
+      "type": "radio",
+      "choices": {
+        "o": "Cones<br>",
+        "u": "Cubes<br>",
+        "b": "Both<br>",
+        "x": "Not Attempted"
+      },
+      "defaultValue": "x"
+    }
+  ],
+  "endgame": [
+    { "name": "Docking Timer",
+      "code": "dt",
+      "type": "timer"
+    },
+    { "name": "Final Status",
+      "code": "fs",
+      "type":"radio",
+      "choices": {
+        "p": "Parked<br>",
+        "d": "Docked (Not Engaged)<br>",
+        "e": "Engaged<br>",
+        "a": "Attempted but failed<br>",
+        "x": "Not attempted"
+      },
+      "defaultValue": "x"
+    },
+    { "name": "Total # of alliance<br>robots docked/engaged",
+      "code": "dn",
+      "type": "counter"
+    }
+  ],
+  "postmatch": [
+    { "name": "Driver Skill",
+      "code": "ds",
+      "type": "radio",
+      "choices": {
+        "n": "Not Effective<br>",
+        "a": "Average<br>",
+        "v": "Very Effective<br>",
+        "x": "Not Observed"
+      },
+      "defaultValue": "x"
+    },
+    { "name": "Links Scored",
+      "code": "ls",
+      "type": "counter"
+    },
+    { "name": "Defense Rating",
+      "code": "dr",
+      "type": "radio",
+      "choices": {
+        "b": "Below Average<br>",
+        "a": "Average<br>",
+        "g": "Good<br>",
+        "e": "Excellent<br>",
+        "x": "Did not play defense"
+      },
+      "defaultValue": "x"
+    },
+    { "name": "Swerve drive?",
+      "code": "sd",
+      "type": "bool"
+    },
+    { "name": "Speed Rating",
+      "code": "sr",
+      "type": "radio",
+      "choices": {
+        "1": "1 (slow)<br>",
+        "2": "2<br>",
+        "3": "3<br>",
+        "4": "4<br>",
+        "5": "5 (fast)"
+      },
+      "defaultValue":"3"
+    },
+    { "name": "Died/Immobilized",
+      "code": "die",
+      "type": "bool"
+    },
+    { "name": "Tippy<br>(almost tipped over)",
+      "code": "tip",
+      "type": "bool"
+    },
+    { "name": "Dropped Cones (>2)",
+      "code": "dc",
+      "type": "bool"
+    },
+    { "name": "Make good<br>alliance partner?",
+      "tooltip": "Would you want this robot on your alliance in eliminations?",
+      "code": "all",
+      "type": "bool"
+    },
+    { "name": "Comments",
+      "code": "co",
+      "type": "text",
+      "size": 15,
+      "maxSize": 50
+    }
+  ]
+}`;
