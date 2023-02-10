7d43b28f44c27d77d81d2c3df3f135bf3c1719e5
micah-wave <124638228+micah-wave@users.noreply.github.com>
2023-02-09 19:40:57 -0500

    Insert preload piece
    inserted at line 58-64
    { "name": "Preload Piece",
          "code": "p",
          "type": "preload",
          "choices": {
            "cb1": "Cube<br>",
            "cn1": "Cone<br>",
          },

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -55,6 +55,13 @@ var config_data = `
       "min": 1,
       "max": 99999
     },
+    { "name": "Preload Piece",
+      "code": "p",
+      "type": "preload",
+      "choices": {
+        "cb1": "Cube<br>",
+        "cn1": "Cone<br>",
+      },
     { "name": "Auto Start Position",
       "code": "as",
       "type": "clickable_image",
